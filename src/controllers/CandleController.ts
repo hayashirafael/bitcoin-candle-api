1f91e2f6a64cd3626fbac544538aa71df818975b
Rafael <rafaelhayashi8@gmail.com>
2023-05-10 14:24:38 -0300

    feat: Consuming the new candles from RabbitMQ and emitting new candles from web socket

---
+++ src/controllers/CandleController.ts
@@ -0,0 +1,20 @@
+import { Candle, CandleModel } from "src/models/CandleModel";
+
+export default class CandleController {
+
+    async save(candle: Candle): Promise<Candle> {
+        const newCandle = await CandleModel.create(candle)
+        return newCandle
+    }
+
+    async findLastCandles(quantity: number): Promise<Candle[]> {
+        const n = quantity > 0 ? quantity : 10
+        const lastCandles: Candle[] =
+            await CandleModel
+                .find()
+                .sort({_id: -1}) //sortear dos ultimos p primeiros
+                .limit(n)
+        return lastCandles;
+
+    }
+}
---
+++ src/messages/CandleMessageChannel.ts
@@ -0,0 +1,56 @@
+import { Channel, connect } from 'amqplib'
+import {config} from 'dotenv'
+import { Server } from 'socket.io'
+import * as http from 'http';
+import CandleController from 'src/controllers/CandleController'
+import { Candle } from 'src/models/CandleModel';
+
+config()
+
+//Funcao da classe
+// Abrir conexao com a fila no rabbitMQ, consumir as msg, gravar no BD e mandar via socket para o front
+export default class CandleMessageChannel {
+    private _channel: Channel
+    private _candleCtrl: CandleController
+    private _io: Server
+
+    constructor(server: http.Server) {
+        this._candleCtrl = new CandleController()
+        this._io = new Server(server, {
+            cors: {
+                origin: process.env.SOCKET_CLIENT_SERVER,
+                methods: ["GET", "POST"]
+            }
+        })
+        this._io.on('connection', () => console.log('Web socket connection created'))
+        this._createMessageChannel()
+    }
+    //criar conexao com rabbitMQ
+    private async _createMessageChannel() {
+        try {
+            const connection = await connect(process.env.AMQP_SERVER)
+            this._channel = await connection.createChannel()
+            this._channel.assertQueue(process.env.QUEUE_NAME)
+        } catch (error) {
+            console.log('Connection to RabbitMQ failed')
+            console.log(error)
+        }
+    }
+
+    //Consumir mensagens
+    consumeMessages() {
+        this._channel.consume(process.env.QUEUE_NAME, async (msg) => {
+            const candleObj = JSON.parse(msg.content.toString())
+            console.log('Message received')
+            console.log(candleObj)
+            this._channel.ack(msg)
+
+            const candle: Candle = candleObj
+            await this._candleCtrl.save(candle)
+            console.log('Candle saved to database')
+            this._io.emit(process.env.SOCKET_EVENT_NAME, candle)
+            console.log('New candle emited by web socket')
+        })
+        console.log('Candle consumer started')
+    }
+}
